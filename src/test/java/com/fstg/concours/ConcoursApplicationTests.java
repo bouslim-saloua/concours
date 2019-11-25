cc65e9565d218baf6cf0142633651c4e6d78987f
Smart Safi <Smart Safi@DESKTOP-Q1A79EC>
2019-11-25 15:04:18 +0100

    aaa

---
+++ src/test/java/com/fstg/concours/ConcoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fstg.concours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
